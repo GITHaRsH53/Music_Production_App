772a696b8d6fb664bcae8d549d839a9de025bdf4
Harsh Thakur <guessmenot2003@gmail.com>
2025-05-16 11:28:01 +0530

    added image and updated home page

---
+++ HARSH_MUSIC_PRODUCTION/HARSH_MUSIC_PRODUCTION/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HARSH_MUSIC_PRODUCTION project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HARSH_MUSIC_PRODUCTION.settings')
+
+application = get_wsgi_application()
